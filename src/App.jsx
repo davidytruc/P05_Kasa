80024bc10d73e68f0669277e67e9c2165c2d80dc
davidytruc <davidytruc@gmail.com>
2025-01-16 08:52:38 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import './App.css'
+import Navigation from "./composants/Navigation"
+import Accueil from "./pages/accueil/Accueil"
+import Apropos from "./pages/apropos/Apropos"
+import Footer from "./composants/footer"
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="accueil" element={<Accueil />} />
+        <Route path="apropos" element={<Apropos />} />
+      </Routes>
+      <Footer />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/accueil/Accueil.jsx
@@ -0,0 +1,10 @@
+import Banner from "../../composants/Banner"
+
+export default function Accueil() {
+    return (
+        <>
+            <Banner />
+            <h1>Ma page accueil</h1>
+        </>
+    )
+}
